5638a692ae0e74c016619523d9bd153b413fac1c
Jani Patrik <85952515+OHOKs@users.noreply.github.com>
2025-03-25 12:21:19 +0100

    Delete src/components/hello.tsx

--- src/components/hello.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Hello() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      Hello World!
-    </div>
-  );
-}

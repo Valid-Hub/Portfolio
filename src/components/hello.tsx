3bd720d7bdf0030979b656a573d7dd40ed9c2a3e
Jani Patrik <85952515+OHOKs@users.noreply.github.com>
2025-03-25 08:44:15 +0100

    Initialize project

---
+++ src/components/hello.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Hello() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      Hello World!
+    </div>
+  );
+}

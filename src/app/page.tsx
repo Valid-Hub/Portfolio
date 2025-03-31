3dd8254dff14c1dac2e71fc2bfce62f2d68a2d88
valid <janipatrik138@gmail.com>
2025-03-31 22:16:12 +0200

    added animation to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,202 +1,197 @@
-"use client";
-import React, { useState, useEffect } from "react";
+'use client';
+import React, { useState, useEffect } from 'react';
 
-import SpecialityCard from "@/components/SpecialityCard";
-import LandingBox from "@/components/LandingBox";
-import Paragraph from "@/components/Paragraph";
-import QualityCubes from "@/components/QualityCubes";
-import ProjectCard from "@/components/ProjectCard";
-import Navbar from "@/components/Navbar";
-import AboutMeDescription from "@/components/AboutMeDescription";
-import CardSlider from "@/components/CardSlider";
+import SpecialityCard from '@/components/SpecialityCard';
+import LandingBox from '@/components/LandingBox';
+import Paragraph from '@/components/Paragraph';
+import QualityCubes from '@/components/QualityCubes';
+import ProjectCard from '@/components/ProjectCard';
+import Navbar from '@/components/Navbar';
+import AboutMeDescription from '@/components/AboutMeDescription';
+import CardSlider from '@/components/CardSlider';
 
-import "@/styles/HomeStyles.css";
+import '@/styles/HomeStyles.css';
 
 export default function Home() {
-  const cardData = [
-    {
-      imageUrl: "alma.jpg",
-      title: "Project 1",
-      content: "Description for Project 1",
-      hasGithub: true,
-      githubUrl: "https://github.com/project1",
-      hasWebsite: true,
-      websiteUrl: "https://project1.com",
-    },
-    {
-      imageUrl: "alma.jpg",
-      title: "Project 2",
-      content: "Description for Project 2",
-      hasGithub: true,
-      githubUrl: "https://github.com/project2",
-      hasWebsite: false,
-    },
-    {
-      imageUrl: "alma.jpg",
-      title: "Project 3",
-      content: "Description for Project 3",
-      hasGithub: false,
-      hasWebsite: true,
-      websiteUrl: "https://project3.com",
-    },
-    {
-      imageUrl: "alma.jpg",
-      title: "Project 4",
-      content: "Description for Project 4",
-      hasGithub: true,
-      githubUrl: "https://github.com/project4",
-      hasWebsite: true,
-      websiteUrl: "https://project4.com",
-    },
-  ];
+    const cardData = [
+        {
+            imageUrl: 'alma.jpg',
+            title: 'Project 1',
+            content: 'Description for Project 1',
+            hasGithub: true,
+            githubUrl: 'https://github.com/project1',
+            hasWebsite: true,
+            websiteUrl: 'https://project1.com',
+        },
+        {
+            imageUrl: 'alma.jpg',
+            title: 'Project 2',
+            content: 'Description for Project 2',
+            hasGithub: true,
+            githubUrl: 'https://github.com/project2',
+            hasWebsite: false,
+        },
+        {
+            imageUrl: 'alma.jpg',
+            title: 'Project 3',
+            content: 'Description for Project 3',
+            hasGithub: false,
+            hasWebsite: true,
+            websiteUrl: 'https://project3.com',
+        },
+        {
+            imageUrl: 'alma.jpg',
+            title: 'Project 4',
+            content: 'Description for Project 4',
+            hasGithub: true,
+            githubUrl: 'https://github.com/project4',
+            hasWebsite: true,
+            websiteUrl: 'https://project4.com',
+        },
+    ];
 
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const updatePosition = (e) => {
-      setPosition({ x: e.clientX, y: e.clientY });
+    useEffect(() => {
+        const updatePosition = (e) => {
+            setPosition({ x: e.clientX, y: e.clientY });
+        };
+
+        window.addEventListener('mousemove', updatePosition);
+        return () => window.removeEventListener('mousemove', updatePosition);
+    }, []);
+
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleResize2 = () => {
+        if (window.innerWidth < 347) {
+            setIsVisible(false);
+        } else {
+            setIsVisible(true);
+        }
     };
 
-    window.addEventListener("mousemove", updatePosition);
-    return () => window.removeEventListener("mousemove", updatePosition);
-  }, []);
+    useEffect(() => {
+        window.addEventListener('resize', handleResize2);
+        handleResize2();
+    });
 
-  const [isVisible, setIsVisible] = useState(false);
+    return (
+        <div>
+            {isVisible && (
+                <div className="page-holder">
+                    <div className="blur-layer"></div>
 
-  const handleResize2 = () => {
-    if (window.innerWidth < 347) {
-      setIsVisible(false);
-    } else {
-      setIsVisible(true);
-    }
-  };
+                    <div
+                        className="mouse-glow-button"
+                        style={{
+                            left: position.x,
+                            top: position.y,
+                        }}
+                    />
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize2);
-    handleResize2();
-  });
+                    <div className="navbar">
+                        <Navbar />
+                    </div>
 
-  return (
-    <div>
-      {isVisible && (
-        <div className="page-holder">
-          <div className="blur-layer"></div>
+                    <div className="home">
+                        <LandingBox />
+                    </div>
 
-          <div
-            className="mouse-glow-button"
-            style={{
-              left: position.x,
-              top: position.y,
-            }}
-          />
+                    <div className="paragraph-holder">
+                        <Paragraph
+                            props={{
+                                category: 'SPECIALITY',
+                                titleFirstWord: 'My',
+                                titleSecondWord: 'Specialities',
+                                content:
+                                    'I specialize in developing efficient and scalable applications using modern technologies. My expertise spans across backend and frontend development.',
+                            }}
+                        />
+                    </div>
 
-          <div className="navbar">
-            <Navbar />
-          </div>
+                    <div className="specialities">
+                        <SpecialityCard
+                            props={{
+                                iconName: 'github',
+                                title: 'Github manager',
+                                content: 'Proficient in managing repositories, and collaborating with teams to maintain efficient codebases.',
+                            }}
+                        />
+                        <SpecialityCard
+                            props={{
+                                iconName: 'settings',
+                                title: 'Fullstack Developer',
+                                content: 'Skilled in both frontend and backend development, creating seamless user experiences.',
+                            }}
+                        />
+                        <SpecialityCard
+                            props={{
+                                iconName: 'home',
+                                title: 'DevOps Enthusiast',
+                                content: 'Beginner in automating workflows and deploying applications to ensure efficient operations.',
+                            }}
+                        />
+                    </div>
 
-          <div className="home">
-            <LandingBox />
-          </div>
+                    <div className="paragraph-holder">
+                        <Paragraph
+                            props={{
+                                category: 'ABOUT ME',
+                                titleFirstWord: 'Key',
+                                titleSecondWord: 'Qualities',
+                                content:
+                                    'I enjoy crafting clean and maintainable code while solving complex problems. My focus is on building intuitive and high-performance applications.',
+                            }}
+                        />
+                    </div>
 
-          <div className="paragraph-holder">
-            <Paragraph
-              props={{
-                category: "SPECIALITY",
-                titleFirstWord: "My",
-                titleSecondWord: "Specialities",
-                content:
-                  "I specialize in developing efficient and scalable applications using modern technologies. My expertise spans across backend and frontend development.",
-              }}
-            />
-          </div>
+                    <div className="about">
+                        <div className="about-qualities">
+                            <div className="first-line">
+                                <QualityCubes props={{ content: 'Creative' }} />
+                                <QualityCubes props={{ content: 'Logical' }} />
+                                <QualityCubes props={{ content: 'Focused' }} />
+                                <QualityCubes props={{ content: 'Precise' }} />
+                            </div>
+                            <div className="second-line">
+                                <QualityCubes props={{ content: 'Efficient' }} />
+                                <QualityCubes props={{ content: 'Innovative' }} />
+                                <QualityCubes props={{ content: 'Reliable' }} />
+                            </div>
+                        </div>
+                        <div className="about-description">
+                            <AboutMeDescription
+                                props={{
+                                    imageUrl: 'alma.jpg',
+                                    title: 'Passionate Developer & Tech Enthusiast',
+                                    partOne:
+                                        'I am Patrik, a high school student and an aspiring full-stack developer with a strong passion for software development. With experience in Vue.js, Express.js, C#, NestJS, and MySQL, I enjoy building scalable applications and optimizing workflows.',
+                                    partTwo:
+                                        'My expertise spans across backend and frontend development, DevOps, and cloud-based deployments. I have worked on projects ranging from admin dashboards to reservation systems, always aiming for efficiency and great user experience.',
+                                }}
+                            />
+                        </div>
+                    </div>
 
-          <div className="specialities">
-            <SpecialityCard
-              props={{
-                iconName: "github",
-                title: "Github manager",
-                content:
-                  "Proficient in managing repositories, and collaborating with teams to maintain efficient codebases.",
-              }}
-            />
-            <SpecialityCard
-              props={{
-                iconName: "settings",
-                title: "Fullstack Developer",
-                content:
-                  "Skilled in both frontend and backend development, creating seamless user experiences.",
-              }}
-            />
-            <SpecialityCard
-              props={{
-                iconName: "home",
-                title: "DevOps Enthusiast",
-                content:
-                  "Beginner in automating workflows and deploying applications to ensure efficient operations.",
-              }}
-            />
-          </div>
+                    <div className="projects">
+                        <CardSlider cards={cardData} />;
+                    </div>
+                    <div className="contact"></div>
+                </div>
+            )}
 
-          <div className="paragraph-holder">
-            <Paragraph
-              props={{
-                category: "ABOUT ME",
-                titleFirstWord: "Key",
-                titleSecondWord: "Qualities",
-                content:
-                  "I enjoy crafting clean and maintainable code while solving complex problems. My focus is on building intuitive and high-performance applications.",
-              }}
-            />
-          </div>
-
-          <div className="about">
-            <div className="about-qualities">
-              <div className="first-line">
-                <QualityCubes props={{ content: "Creative" }} />
-                <QualityCubes props={{ content: "Logical" }} />
-                <QualityCubes props={{ content: "Focused" }} />
-                <QualityCubes props={{ content: "Precise" }} />
-              </div>
-              <div className="second-line">
-                <QualityCubes props={{ content: "Efficient" }} />
-                <QualityCubes props={{ content: "Innovative" }} />
-                <QualityCubes props={{ content: "Reliable" }} />
-              </div>
-            </div>
-            <div className="about-description">
-              <AboutMeDescription
-                props={{
-                  imageUrl: "alma.jpg",
-                  title: "Passionate Developer & Tech Enthusiast",
-                  partOne:
-                    "I am Patrik, a high school student and an aspiring full-stack developer with a strong passion for software development. With experience in Vue.js, Express.js, C#, NestJS, and MySQL, I enjoy building scalable applications and optimizing workflows.",
-                  partTwo:
-                    "My expertise spans across backend and frontend development, DevOps, and cloud-based deployments. I have worked on projects ranging from admin dashboards to reservation systems, always aiming for efficiency and great user experience.",
-                }}
-              />
-            </div>
-          </div>
-
-          <div className="projects">
-            <CardSlider cards={cardData} />;
-          </div>
-          <div className="contact"></div>
+            {!isVisible && (
+                <div className="screen">
+                    <div className="text">The screen is not large enough to view this page!</div>
+                </div>
+            )}
         </div>
-      )}
-
-      {!isVisible && (
-        <div className="screen">
-          <div className="text">
-            The screen is not large enough to view this page!
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 {
-  /*
+    /*
     const cardData = [
       {
         imageUrl: "alma.jpg",
@@ -239,19 +234,19 @@ export default function Home() {
 }
 
 {
-  /*
+    /*
    <Navbar />
   */
 }
 
 {
-  /*
+    /*
     <LandingBox />
   */
 }
 
 {
-  /*
+    /*
     <AboutMeDescription
       props={{
         imageUrl: "alma.jpg",
@@ -266,7 +261,7 @@ export default function Home() {
 }
 
 {
-  /*
+    /*
     <ProjectCard
       props={{
         imageUrl: "alma.jpg",
@@ -284,13 +279,13 @@ export default function Home() {
 }
 
 {
-  /*
+    /*
     <QualityCubes props={{ content: "Creative" }} />
   */
 }
 
 {
-  /*
+    /*
     <Paragraph
       props={{
         category: "SPECIALITY",
@@ -304,7 +299,7 @@ export default function Home() {
 }
 
 {
-  /*
+    /*
   <SpecialityCard props={{ iconName : "github", title: "Github manager", content: "Proficient in managing repositories, and collaborating with teams to maintain efficient codebases." }} />
   <SpecialityCard props={{ iconName : "settings", title: "Fullstack Developer", content: "Skilled in both frontend and backend development, creating seamless user experiences." }} />
   <SpecialityCard props={{ iconName : "home", title: "DevOps Enthusiast", content: "Beginner in automating workflows and deploying applications to ensure efficient operations." }} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,66 +1,55 @@
-"user client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import "@/styles/NavbarStyles.css";
+import '@/styles/NavbarStyles.css';
 
-import Icon from "@mdi/react";
-import { mdiMenu } from "@mdi/js";
+import Icon from '@mdi/react';
+import { mdiMenu } from '@mdi/js';
 
 export default function Navbar() {
-  const [isLargeEnough, setIsLargeEnough] = useState(false);
+    const [isLargeEnough, setIsLargeEnough] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const handleResize3 = () => {
-    if (window.innerWidth < 600) {
-      setIsLargeEnough(false);
-    } else {
-      setIsLargeEnough(true);
-    }
-  };
+    const handleResize = () => {
+        setIsLargeEnough(window.innerWidth >= 600);
+        if (window.innerWidth >= 600) setIsOpen(false);
+    };
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize3);
-    handleResize3();
-  });
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const setOpen = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div className="bar">
-      <div className="holder">
-        {isLargeEnough && (
-          <div className="monitor">
-            <a href="">Home</a>
-            <a href="">Specialities</a>
-            <a href="">About</a>
-            <a href="">Projects</a>
-            <a href="">Contact</a>
-          </div>
-        )}
-        {!isLargeEnough && (
-          <div className="mobile-open">
-            <div className="menu-open">
-              <div className="title">Portfolio</div>
-              <div className="button" onClick={setOpen}>
-                <Icon path={mdiMenu} size={1} />
-              </div>
+    return (
+        <div className="bar">
+            <div className="holder">
+                {isLargeEnough ? (
+                    <div className="monitor">
+                        <a href="">Home</a>
+                        <a href="">Specialities</a>
+                        <a href="">About</a>
+                        <a href="">Projects</a>
+                        <a href="">Contact</a>
+                    </div>
+                ) : (
+                    <div className="mobile-open">
+                        <div className="menu-open">
+                            <div className="title">Portfolio</div>
+                            <div className="button" onClick={() => setIsOpen(!isOpen)}>
+                                <Icon path={mdiMenu} size={1} />
+                            </div>
+                        </div>
+                        <div className={`menu-items ${isOpen ? 'open' : ''}`}>
+                            <div className="line"></div>
+                            <a href="">Home</a>
+                            <a href="">Specialities</a>
+                            <a href="">About</a>
+                            <a href="">Projects</a>
+                            <a href="">Contact</a>
+                        </div>
+                    </div>
+                )}
             </div>
-            {isOpen && (
-              <div className="menu-items">
-                <div className="line"></div>
-                <a href="">Home</a>
-                <a href="">Specialities</a>
-                <a href="">About</a>
-                <a href="">Projects</a>
-                <a href="">Contact</a>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }

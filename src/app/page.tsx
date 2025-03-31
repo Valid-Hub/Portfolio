aa6471dddd7a1eccf0743162d534788288fecdf5
Jani Patrik <85952515+OHOKs@users.noreply.github.com>
2025-03-31 12:36:25 +0200

    fixed some responsivity problems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,109 +61,136 @@ export default function Home() {
     return () => window.removeEventListener("mousemove", updatePosition);
   }, []);
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleResize2 = () => {
+    if (window.innerWidth < 347) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize2);
+    handleResize2();
+  });
+
   return (
-    <div className="page-holder">
-      <div className="blur-layer"></div>
+    <div>
+      {isVisible && (
+        <div className="page-holder">
+          <div className="blur-layer"></div>
 
-      <div
-        className="mouse-glow-button"
-        style={{
-          left: position.x,
-          top: position.y,
-        }}
-      />
-
-      <div className="navbar">
-        <Navbar />
-      </div>
-
-      <div className="home">
-        <LandingBox />
-      </div>
-
-      <div className="paragraph-holder">
-        <Paragraph
-          props={{
-            category: "SPECIALITY",
-            titleFirstWord: "My",
-            titleSecondWord: "Specialities",
-            content:
-              "I specialize in developing efficient and scalable applications using modern technologies. My expertise spans across backend and frontend development.",
-          }}
-        />
-      </div>
-
-      <div className="specialities">
-        <SpecialityCard
-          props={{
-            iconName: "github",
-            title: "Github manager",
-            content:
-              "Proficient in managing repositories, and collaborating with teams to maintain efficient codebases.",
-          }}
-        />
-        <SpecialityCard
-          props={{
-            iconName: "settings",
-            title: "Fullstack Developer",
-            content:
-              "Skilled in both frontend and backend development, creating seamless user experiences.",
-          }}
-        />
-        <SpecialityCard
-          props={{
-            iconName: "home",
-            title: "DevOps Enthusiast",
-            content:
-              "Beginner in automating workflows and deploying applications to ensure efficient operations.",
-          }}
-        />
-      </div>
-
-      <div className="paragraph-holder">
-        <Paragraph
-          props={{
-            category: "ABOUT ME",
-            titleFirstWord: "Key",
-            titleSecondWord: "Qualities",
-            content:
-              "I enjoy crafting clean and maintainable code while solving complex problems. My focus is on building intuitive and high-performance applications.",
-          }}
-        />
-      </div>
-
-      <div className="about">
-        <div className="about-qualities">
-          <div className="first-line">
-            <QualityCubes props={{ content: "Creative" }} />
-            <QualityCubes props={{ content: "Logical" }} />
-            <QualityCubes props={{ content: "Focused" }} />
-            <QualityCubes props={{ content: "Precise" }} />
-          </div>
-          <div className="second-line">
-            <QualityCubes props={{ content: "Efficient" }} />
-            <QualityCubes props={{ content: "Innovative" }} />
-            <QualityCubes props={{ content: "Reliable" }} />
-          </div>
-        </div>
-        <div className="about-description">
-          <AboutMeDescription
-            props={{
-              imageUrl: "alma.jpg",
-              title: "Passionate Developer & Tech Enthusiast",
-              partOne:
-                "I am Patrik, a high school student and an aspiring full-stack developer with a strong passion for software development. With experience in Vue.js, Express.js, C#, NestJS, and MySQL, I enjoy building scalable applications and optimizing workflows.",
-              partTwo:
-                "My expertise spans across backend and frontend development, DevOps, and cloud-based deployments. I have worked on projects ranging from admin dashboards to reservation systems, always aiming for efficiency and great user experience.",
+          <div
+            className="mouse-glow-button"
+            style={{
+              left: position.x,
+              top: position.y,
             }}
           />
-        </div>
-      </div>
 
-      <div className="projects">
-        <CardSlider cards={cardData} />;
-      </div>
-      <div className="contact"></div>
+          <div className="navbar">
+            <Navbar />
+          </div>
+
+          <div className="home">
+            <LandingBox />
+          </div>
+
+          <div className="paragraph-holder">
+            <Paragraph
+              props={{
+                category: "SPECIALITY",
+                titleFirstWord: "My",
+                titleSecondWord: "Specialities",
+                content:
+                  "I specialize in developing efficient and scalable applications using modern technologies. My expertise spans across backend and frontend development.",
+              }}
+            />
+          </div>
+
+          <div className="specialities">
+            <SpecialityCard
+              props={{
+                iconName: "github",
+                title: "Github manager",
+                content:
+                  "Proficient in managing repositories, and collaborating with teams to maintain efficient codebases.",
+              }}
+            />
+            <SpecialityCard
+              props={{
+                iconName: "settings",
+                title: "Fullstack Developer",
+                content:
+                  "Skilled in both frontend and backend development, creating seamless user experiences.",
+              }}
+            />
+            <SpecialityCard
+              props={{
+                iconName: "home",
+                title: "DevOps Enthusiast",
+                content:
+                  "Beginner in automating workflows and deploying applications to ensure efficient operations.",
+              }}
+            />
+          </div>
+
+          <div className="paragraph-holder">
+            <Paragraph
+              props={{
+                category: "ABOUT ME",
+                titleFirstWord: "Key",
+                titleSecondWord: "Qualities",
+                content:
+                  "I enjoy crafting clean and maintainable code while solving complex problems. My focus is on building intuitive and high-performance applications.",
+              }}
+            />
+          </div>
+
+          <div className="about">
+            <div className="about-qualities">
+              <div className="first-line">
+                <QualityCubes props={{ content: "Creative" }} />
+                <QualityCubes props={{ content: "Logical" }} />
+                <QualityCubes props={{ content: "Focused" }} />
+                <QualityCubes props={{ content: "Precise" }} />
+              </div>
+              <div className="second-line">
+                <QualityCubes props={{ content: "Efficient" }} />
+                <QualityCubes props={{ content: "Innovative" }} />
+                <QualityCubes props={{ content: "Reliable" }} />
+              </div>
+            </div>
+            <div className="about-description">
+              <AboutMeDescription
+                props={{
+                  imageUrl: "alma.jpg",
+                  title: "Passionate Developer & Tech Enthusiast",
+                  partOne:
+                    "I am Patrik, a high school student and an aspiring full-stack developer with a strong passion for software development. With experience in Vue.js, Express.js, C#, NestJS, and MySQL, I enjoy building scalable applications and optimizing workflows.",
+                  partTwo:
+                    "My expertise spans across backend and frontend development, DevOps, and cloud-based deployments. I have worked on projects ranging from admin dashboards to reservation systems, always aiming for efficiency and great user experience.",
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="projects">
+            <CardSlider cards={cardData} />;
+          </div>
+          <div className="contact"></div>
+        </div>
+      )}
+
+      {!isVisible && (
+        <div className="screen">
+          <div className="text">
+            The screen is not large enough to view this page!
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/LandingBox.tsx
+++ src/components/LandingBox.tsx
@@ -1,21 +1,41 @@
-import '@/styles/LandingBoxStyles.css';
+"use client";
+import React, { useState, useEffect } from "react";
+
+import "@/styles/LandingBoxStyles.css";
 
 export default function LandingBox() {
-    return (
-        <div className="box">
-            <div className="left">
-                <div className="title">
-                    <b>Jani Patrik</b>
-                </div>
-                <div className="content">
-                    A passionate and dedicated software developer with experience in full-stack development, DevOps, and GitHub project management. I enjoy
-                    building scalable applications, optimizing workflows, and contributing to open-source projects.
-                </div>
-                <div className="button-holder">
-                    <button className="button">View Projects</button>
-                </div>
-            </div>
-            <div className="right"></div>
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+  });
+
+  return (
+    <div className="box">
+      <div className="left">
+        <div className="title">
+          <b>Jani Patrik</b>
         </div>
-    );
+        <div className="content">
+          A passionate and dedicated software developer with experience in
+          full-stack development, DevOps, and GitHub project management. I enjoy
+          building scalable applications, optimizing workflows, and contributing
+          to open-source projects.
+        </div>
+        <div className="button-holder">
+          <button className="button">View Projects</button>
+        </div>
+      </div>
+      {!isMobile && <div className="right"></div>}
+    </div>
+  );
 }
